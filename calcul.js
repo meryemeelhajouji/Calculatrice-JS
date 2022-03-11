300c9252774263c2907075a6387e534d7993bedd
meryemeelhajouji <meryemelhajouji.99@gmail.com>
2022-03-11 10:29:04 +0100

    js

--- calcul.js
+++ calcul.js
@@ -1,30 +1,129 @@
+function addition(first_number,last_number){
 
- function afficher(val) 
- { 
-     document.getElementById("output").value+=val 
- } 
-
- function effacer() 
- { 
-     document.getElementById("output").value = "" 
- } 
-
- function calculer() 
- { 
-     let a = document.getElementById("output").value 
-     let b = eval(a) 
-     document.getElementById("output").value = b 
- } 
+    return first_number + last_number ;
  
- function additionner (nomber1,nomber2){
-    return nomber1+nomber2;
- }
- function soustraire  (nomber1,nomber2){
-    return nomber1-nomber2;
- }
- function multiplier  (nomber1,nomber2){
-    return nomber1*nomber2;
- }
- function diviser (nomber1,nomber2){
-    return nomber1/nomber2;
- }
+}
+function soustraction(first_number,last_number){
+
+    return first_number - last_number ;
+ 
+}
+function multiplication(first_number,last_number){
+
+    return first_number *  last_number ;
+ 
+}
+function diivision(first_number,last_number){
+
+    return first_number /  last_number ;
+ 
+}
+
+  let operate =  function (first_number,operateur,last_number)  {
+     
+  switch(operateur){
+      case'+':
+      return addition (first_number,last_number);
+      break;
+      case'-':
+      return soustraction (first_number,last_number);
+      break;
+      case'*':
+      return multiplication (first_number,last_number);
+      break;
+      case'/':
+      return diivision (first_number,last_number);
+      break;
+      default:
+       //   console.log("mouchkila")
+  }
+}
+
+
+
+let nomber1 = "0"
+let nomber2 = ""
+let operater =""
+
+
+
+let ecran = document.querySelector('#scran')
+
+let les_number = document.querySelectorAll('.number')
+les_number.forEach((number) => {
+   number.addEventListener('click' , () =>{
+       if(operater===""){
+           nomber1 = number.innerHTML
+           ecran.innerHTML += nomber1 
+       }else{
+           nomber2 = number.innerHTML
+           ecran.innerHTML += nomber2
+
+       }
+   })
+
+})
+
+let operation = document.querySelectorAll('.operation')
+operation.forEach((operation) => {
+    operation.addEventListener('click' , () =>{
+        if(nomber2===""){
+            operater = operation.innerHTML
+          
+        
+        }else{
+            nomber1 =operate(Number(nomber1),operater,Number(nomber2)) 
+            
+            nomber2=""
+           
+        }
+        ecran.innerHTML = `${nomber1} ${operater} ${nomber2}`
+    })
+})
+
+let egale = document.querySelector('#egale')
+egale.addEventListener('click' , () => {
+
+    ecran.innerHTML = operate(Number(nomber1),operater,Number(nomber2)) 
+} )
+
+let effacer = document.querySelector('#effacer')
+
+effacer.addEventListener('click' , () => {
+  
+    nomber1 = ""
+    nomber2 = ""
+    operater = ""
+    ecran.innerHTML = `${nomber1} ${operater} ${nomber2}`
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
